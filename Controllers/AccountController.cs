412a38ef8999a2d4bd52afc3b4da4c7db40a5c09
onyekachila <onyekachila@gmail.com>
2019-12-16 15:43:03 +0100

    Initial commit with login, registeration, token generation

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NG_Auth.Helpers;
+using NG_Auth.Models;
+
+namespace NG_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly SignInManager<IdentityUser> _signManager;
+
+        private readonly AppSettings _appSettings;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+           SignInManager<IdentityUser> signInManager, IOptions<AppSettings> appSettings)
+        {
+            _userManager = userManager;
+            _signManager = signInManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel formdata)
+        {
+            // Will hold all the errors related to registration
+            List<string> errorList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                Email = formdata.Email,
+                UserName = formdata.UserName,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formdata.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                // Sending Confirmation Email
+
+                //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { UserId = user.Id, Code = code }, protocol: HttpContext.Request.Scheme);
+
+                //await _emailsender.SendEmailAsync(user.Email, "Techhowdy.com - Confirm Your Email", "Please confirm your e-mail by clicking this link: <a href=\"" + callbackUrl + "\">click here</a>");
+
+                return Ok(new { username = user.UserName, email = user.Email, status = 1, message = "Registration Successful" });
+
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(errorList));
+
+        }
+
+        // Login Method
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel formdata)
+        {
+            // Get the User from Database
+            var user = await _userManager.FindByNameAsync(formdata.Username);
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings.Secret));
+
+            double tokenExpiryTime = Convert.ToDouble(_appSettings.ExpireTime);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, formdata.Password))
+            {
+
+                //// THen Check If Email Is confirmed
+                //if (!await _userManager.IsEmailConfirmedAsync(user))
+                //{
+                //    ModelState.AddModelError(string.Empty, "User Has not Confirmed Email.");
+
+                //    return Unauthorized(new { LoginError = "We sent you an Confirmation Email. Please Confirm Your Registration With Techhowdy.com To Log in." });
+                //}
+
+                // get user Role
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, formdata.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.NameIdentifier, user.Id),
+                        new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                        new Claim("LoggedOn", DateTime.Now.ToString()),
+
+                     }),
+
+                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = _appSettings.Site,
+                    Audience = _appSettings.Audience,
+                    Expires = DateTime.UtcNow.AddMinutes(tokenExpiryTime)
+                };
+
+                // Generate Token
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new { token = tokenHandler.WriteToken(token), expiration = token.ValidTo, username = user.UserName, userRole = roles.FirstOrDefault() });
+
+            }
+
+            // return error
+            ModelState.AddModelError("", "Username/Password was not Found");
+            return Unauthorized(new { LoginError = "Please Check the Login Credentials - Ivalid Username/Password was entered" });
+
+        }
+
+
+
+
+    }
+}
---
+++ Models/BankModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NG_Auth.Models
+{
+    public class BankModel
+    {
+        [Key]
+        public int BankId { get; set; }
+        public string BankName { get; set; }
+    }
+}
